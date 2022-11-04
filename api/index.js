f80282a188b8b19315c421cdf70c4d2aff83eb58
ljurecki <libette.jurecki@gmail.com>
2022-11-03 22:44:03 -0400

    installed JWT, bcrypt, react, react-dom, react-script, and jsonwebtoken

--- api/index.js
+++ api/index.js
@@ -1,5 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+const { getUserById } = require('../db/users');
+
+router.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
+router.get('/health', async (req, res) => {
+  res.send({
+    message: 'All is well',
+  });
+});
 
 const usersRouter = require('./usersRouter');
 router.use('/users', usersRouter);
@@ -7,9 +44,18 @@ router.use('/users', usersRouter);
 const productsRouter = require ('./productsRouter');
 router.use('/products', productsRouter);
 
+// const audiobooksRouter = require ('./audiobooksRouter');
+// router.use('/audiobooks', audiobooksRouter);
+
 router.get('*', (req, res) => {
     res.status(404).send({
       message: 'Page not found',
+    });
+  });
+
+  router.use((err, req, res) => {
+    res.send({
+      message: err,
     });
   });
 
