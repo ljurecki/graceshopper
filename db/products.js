b05608327acb7c27ab21701a189ce367821486d0
adamerwin44 <99776356+adamerwin44@users.noreply.github.com>
2022-11-06 17:29:41 -0500

    adam sunday night

--- db/products.js
+++ db/products.js
@@ -1,31 +1,84 @@
+import React, { useState, useEffect} from 'react';
+import { useParams, Link, Navigate } from 'react-router-dom';
+//use bootstrap
+import {
+    Card,
+    ListGroup,
+    Row,
+    Col,
+    Button,
+    Form,
+    FloatingLabel,
+    Alert,
+  } from 'react-bootstrap';
 const { client } = require("./client");
-
-async function createProduct({ title, description }) {
-  try {
-    const { rows: [product] } = await client.query(`
-      INSERT INTO products (title, "imageURL", description, price, author, genre, )
-      VALUES ($1, $2, $3, $4, $5)
-      ON CONFLICT (title) DO NOTHING
-      RETURNING *;
-    `, [title, imageURL, description, price, author, genre])
-    return product;
-  } catch (error) {
-  throw error;
-}
-}
+//import from api
+import { 
+    getAllProducts,
+    createProduct,
+    getProductByTitle,
+    getProductByID,
+    getProductByAuthor,
+    getProductByGenre,
+    getProductByPrice,
+    updateProduct,
+    addProductToCart,
+    productAvailability,
+} from '../api';
 
 async function getAllProducts() {
-  try {
-    const { rows: products } = await client.query(
-      `
-      SELECT * FROM products;
-      `
-    );
-    return products;
-  } catch (error) {
-    throw error;
+    try {
+      const { rows: products } = await client.query(
+        `
+        SELECT * FROM products;
+        `
+      );
+      return products;
+    } catch (error) {
+      throw error;
+    }
   }
-}
+
+  async function createProduct() {
+    const newActivity = {
+      name,
+      description,
+      title,
+      genre,
+      price
+    };
+    const result = await createProduct(jwt, user, newActivity);
+    if (result.error) {
+      console.error(result.error);
+      setErrorMessage(result.error);
+    } else {
+      setSuccessMessage('Product Created!');
+      setErrorMessage('');
+      setTimeout(() => {
+        handleClose();
+      }, 1000);
+    }
+  }
+
+async function getProductByTitle(productTitle) {
+    try {
+      const {
+        rows: [product],
+      } = await client.query(
+        `
+        SELECT *
+        FROM products
+        WHERE id = $1;
+        `,
+        [productTitle]
+      );
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
 async function getProductById(productId) {
   try {
     const {
@@ -43,7 +96,64 @@ async function getProductById(productId) {
     throw error;
   }
 }
-async function updateProducts({ id, ...fields }) {
+
+async function getProductByAuthor(productAuthor) {
+    try {
+      const {
+        rows: [product],
+      } = await client.query(
+        `
+        SELECT *
+        FROM products
+        WHERE id = $1;
+        `,
+        [productAuthor]
+      );
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  async function getProductByGenre(productGenre) {
+    try {
+      const {
+        rows: [product],
+      } = await client.query(
+        `
+        SELECT *
+        FROM products
+        WHERE id = $1;
+        `,
+        [productGenre]
+      );
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getProductByPrice(productPrice) {
+    try {
+      const {
+        rows: [product],
+      } = await client.query(
+        `
+        SELECT *
+        FROM products
+        WHERE id = $1;
+        `,
+        [productPrice]
+      );
+      return product;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+async function updateProduct({ id, ...fields }) {
   try {
     const indexString = Object.keys(fields).map((key, index) => {
       return `"${key}"=$${index + 1}`;
@@ -83,11 +193,20 @@ async function deleteProduct(productId) {
   }
 }
 module.exports = {
-  createProduct,
+  
+  
   getAllProducts,
+  createProduct,
+ getProductByTitle,
   getProductById,
-  updateProducts,
-  deleteProduct,
+  getProductByAuthor,
+  getProductByGenre,
+  getProductByPrice,
+
+  updateProduct,
+  addProductToCart,
+  productAvailability,
+  deleteProduct
 };
 // module.exports = {
 //     createProduct
