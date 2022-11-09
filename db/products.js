30124197fb7b2376d15ac8058302157503dc6b88
adamerwin44 <99776356+adamerwin44@users.noreply.github.com>
2022-11-08 19:14:20 -0500

    adam tuesday

--- db/products.js
+++ db/products.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import { useParams, Link, Navigate } from 'react-router-dom';
-//use bootstrap
+//how to use bootstrap
 import {
     Card,
     ListGroup,
