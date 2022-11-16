0840437e786ec1876e7aee10edcbf82272998ff8
Tyler Barthelemy <tbarthelemy12@Tylers-MBP.attlocal.net>
2022-11-15 20:03:12 -0600

    fully merge

--- Src/components/index.js
+++ Src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Navbar } from './NavBar';
+export { default as CreateAudioBooks } from './CreateAudioBooks'
+export { default as CreateProducts } from './CreateProducts'
+export { default as EditAudioBook } from './EditAudioBook'
+export { default as EditProduct } from './EditProduct'
+export { default as LoginForm } from './LoginForm'
