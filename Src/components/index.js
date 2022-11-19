b0df72674ba3d7d3be22a4759f5ab538671e47ba
ljurecki <libette.jurecki@gmail.com>
2022-11-19 14:09:47 -0500

    Updated, Still Working

--- Src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./NavBar"
-// export { default as Cart } from "./Cart"
-export { default as CreateProduct} from "./CreateProduct"
-export { default as EditProduct} from "./EditProduct"
