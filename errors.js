436a2a1012daaadc26fe28f9e35d2e61474c1b28
ljurecki <106923097+ljurecki@users.noreply.github.com>
2022-11-19 11:36:30 -0500

    Update errors.js

--- errors.js
+++ errors.js
@@ -1,8 +1,6 @@
 module.exports = {
     ProductExistsError: (title) => `A Product with title ${title} already exists`,
     ProductNotFoundError: (id) => `Product ${id} not found`,
-    AudiobookExistsError: (title) => `An Audiobook with title ${title} already exists`,
-    AudiobookNotFoundError: (id) => `Audiobook ${id} not found`,
     UnauthorizedError: () => "You must be logged in to perform this action",
     UserDoesNotExistError: (name) => `User ${name} does not exist`,
     PasswordTooShortError: () => `Password Too Short!`,
