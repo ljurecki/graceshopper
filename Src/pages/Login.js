2e7a2d291d3464aa97064f3f7e33fbd24a7f3378
ljurecki <106923097+ljurecki@users.noreply.github.com>
2022-11-19 11:40:20 -0500

    Update Login.js

---
+++ Src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <>
+            <div id="title">
+                <h1>
+                    Login!
+                </h1>
+            </div>
+        </>
+    )
+};
+
+export default Login;
