ab12e0c7120ec20149ff6d15dc3426e9468a5ecd
mandybrandt <mandybrandt2001@yahoo.com>
2022-11-20 13:21:27 -0700

    commented out routehandelr

--- routehandler.js
+++ routehandler.js
@@ -1,6 +1,6 @@
-const productRoutes = require("./Product/routes");
-const cartRoutes = require('./Cart/routes')
-module.exports = app => {
-    app.use("/product", productRoutes);
-    app.use("/cart", cartRoutes);
-}
+// const productRoutes = require("./Product/routes");
+// const cartRoutes = require('./Cart/routes')
+// module.exports = app => {
+//     app.use("/product", productRoutes);
+//     app.use("/cart", cartRoutes);
+// }
