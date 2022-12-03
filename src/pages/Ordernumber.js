297686ba623b85d859a64b4ef041dfeb04ae0768
Andreessen1 <106788431+Andreessen1@users.noreply.github.com>
2022-12-03 11:38:58 -0500

    Update Ordernumber.js

--- src/pages/Ordernumber.js
+++ src/pages/Ordernumber.js
@@ -1,5 +1,6 @@
 import React from 'react';
 <link rel="stylesheet" href="checkout.css"></link>
+import './checkout.css';
 function getRandomInt() {
     return Math.floor(Math.random() * 500  + 10000);
   }
@@ -8,13 +9,16 @@ const Ordernumber = ({time}) => {
     return (
         <>
             <div id="address">
-                <h1>
+                <h1 id="orderNumber">
                     Your Order Number is
                 </h1>
-              <h3>
+              <h3 id="orderNumberReturn">
              {getRandomInt() + "BB"}
-              </h3>
-            </div>
+             </h3>
+            
+              
+              <img src="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+     />       </div>
         </>
     )
 };
