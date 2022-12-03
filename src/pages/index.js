ecf71fa05b23a0e2037bf082bd4a8af6edc3f697
mandybrandt <mandybrandt2001@yahoo.com>
2022-12-03 10:42:48 -0700

    this time

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,7 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as Products } from './Products';
 export { default as Cart } from './Cart';
-<<<<<<< HEAD
-export { default as Ordernumber } from './Ordernumber';
-=======
 export { default as Checkout } from './Checkout';
 export {default as Ordernumber} from './Ordernumber'
 
->>>>>>> 81ae97df20831b3a4ecb437ac69d40d639714793
 
