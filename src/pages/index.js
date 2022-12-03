2d0747c808447c46be8ba90163acb1cbcb12d69e
mandybrandt <mandybrandt2001@yahoo.com>
2022-12-03 10:18:14 -0700

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as Products } from './Products';
 export { default as Cart } from './Cart';
-
+export { default as Ordernumber } from './Ordernumber';
 
