a9f6477b26a4a84134b3499afaa1135d0c6b9d16
ljurecki <106923097+ljurecki@users.noreply.github.com>
2022-12-04 19:19:51 -0500

    Update Checkout.js

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,10 +1,119 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-<link rel="stylesheet" href="checkout.css"></link>
-import './checkout.css';
+import { Form, Button, FloatingLabel, Tabs, Tab, Container } from 'react-bootstrap';
+
+const Checkout = ({ navigate }) => {
+
+    return (
+        <>
+            <Tabs
+                justify='true'
+                variant='pills'
+                className='bg-dark'
+                style={{ fontSize: '60px' }}>
+                <Tab eventKey="Login" title="Almost Yours, Checkout!"></Tab>
+            </Tabs>
+            <Container>
+                <Form>
+                    <Form.Group className='m-3 mt-5'>
+                        <Form.Label><h1>Shipping Information</h1></Form.Label>
+                        <FloatingLabel label='Full Name'>
+                            <Form.Control
+                                style={{ width: '40%' }}
+                                placeholder='Full Name' />
+                        </FloatingLabel>
+                    </Form.Group>
+
+                    <Form.Group className='m-3'>
+                        <FloatingLabel label='Address Line 1'>
+                            <Form.Control
+                                style={{ width: '75%' }}
+                                placeholder='Address1' />
+                        </FloatingLabel>
+                    </Form.Group>
+
+                    <Form.Group className='m-3'>
+
+                        <FloatingLabel label='Address Line 2'>
+                            <Form.Control
+                                style={{ width: '75%' }}
+                                placeholder='Address2' />
+                        </FloatingLabel>
+                    </Form.Group>
+
+                    <Form.Group className='m-3'>
+                        <FloatingLabel label='City'>
+                            <Form.Control
+                            style={{ width: '25%' }}
+                                placeholder='City' />
+                        </FloatingLabel>
+                    </Form.Group>
+
+                    <Form.Group className='m-3'>
+                        <FloatingLabel label='State'>
+                            <Form.Control
+                            style={{ width: '25%' }}
+                                placeholder='State' />
+                        </FloatingLabel>
+                    </Form.Group>
+
+                    <Form.Group className='m-3'>
+                        <FloatingLabel label='Zipcode'>
+                            <Form.Control
+                                id='zipcode'
+                                style={{ width: '10%' }}
+                                placeholder='zipcode' />
+                        </FloatingLabel>
+                    </Form.Group>
+
+                    <Form.Group className='m-3 mt-5'>
+                        <Form.Label><h1>Billing Information</h1></Form.Label>
+
+                        <Form.Group className='m-3'>
+                            <FloatingLabel label='Card Number'>
+                                <Form.Control
+                                style={{ width: '40%' }}
+                                    placeholder='Card Number' />
+                            </FloatingLabel>
+                        </Form.Group>
+
+                        <Form.Group className='m-3'>
+                            <FloatingLabel label='CVC'>
+                                <Form.Control
+                                style={{ width: '40%' }}
+                                    placeholder='cvc' />
+                            </FloatingLabel>
+                        </Form.Group>
+
+                        <Form.Group className='m-3'>
+                            <FloatingLabel label='Expiration Code'>
+                                <Form.Control
+                                style={{ width: '40%' }}
+                                    placeholder='Expiration Code' />
+                            </FloatingLabel>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3 mx-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Billing Address Same as Shipping Address" defaultChecked={true} />
+                        </Form.Group>
+                        <Button className="mb-3 mx-3" variant="primary"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                navigate('/Ordernumber')
+                            }}>
+                            Submit Order
+                        </Button>
+                    </Form.Group>
+                </Form>
+            </Container>
+        </>
+    );
+};
+
+export default Checkout;
 
 
-const Checkout = () => {
+{/* 
+
     return (
         <>
             <div id="address">
@@ -12,27 +121,27 @@ const Checkout = () => {
                     Billing Address
                 </h1>
                 <form id="addressOne">
-                    <label>Name </label><input type="text" required/>
-                    <label>Address Line 1: </label> <input type="text" required/>
-                    <label>State </label><input type="text" required/>
-                    <label>Zipcode </label><input type="text" required/>
+                    <label>name <input type="text"/></label>
+                    <label>Address Line 1: <input type="text"/></label>
+                    <label>State <input type="text"/></label>
+                    <label>Zipcode <input type="text"/></label>
                 </form>
                 <h1>
                     Credit Card/Debit Card Info
                 </h1>
                 <form id="addressTwo">
-                <label>Name </label> <input type="text"/>
-                    <label>Address Line 1: </label><input type="text" required/>
-                    <label>State </label><input type="text" required/>
-                    <label>Zipcode </label><input type="text"required/>
-                    <label>Card Number </label><input type="text"required/>
-                    <label>CVC </label><input type="text" required/>
-                    <label>Experation Date </label><input type="text" required/>
+                <label>name <input type="text"/></label>
+                    <label>Address Line 1: <input type="text"/></label>
+                    <label>State <input type="text"/></label>
+                    <label>Zipcode <input type="text"/></label>
+                    <label>Card Number <input type="text"/></label>
+                    <label>CVC <input type="text"/></label>
+                    <label>Experation Date <input type="text"/></label>
                 </form>
-                <Link to="/Ordernumber">Submit Order</Link>
+                <Link to="/Ordernumber">Submit</Link>
             </div>
         </>
     )
-};
+}; */}
 
-export default Checkout;
+
