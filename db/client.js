dd4d23f29cc15e4e9e8bd261d5621796b8ae7c9c
ljurecki <libette.jurecki@gmail.com>
2022-12-06 21:26:35 -0500

    Base_URL updated to render url

--- db/client.js
+++ db/client.js
@@ -3,7 +3,7 @@
 const { Client } = require('pg');
 
 const connectionString =
-  process.env.DATABASE_URL || 'postgres://bestbooks_h06d_user:e3VPVUPfFZhCuh0M4PZpfbK0jkJ9HUSv@dpg-ce7u71p4reber9d2igl0-a/bestbooks_h06d';
+  process.env.DATABASE_URL || 'postgress://localhost:5432/graceshopper';
 
 
 const client = new Client({
