4f6be0b341661ab52fb125af50d1c78aafaf8a41
mandybrandt <mandybrandt2001@yahoo.com>
2022-12-08 20:05:47 -0700

    updated formatting

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,13 +1,5 @@
 import React, { useState } from 'react';
 import { addProductToCart, deleteProduct } from '../api';
-import { Card, ListGroup, Button, Tab, Tabs, Container, Row, Col, Modal, Alert} from 'react-bootstrap';
-import {EditProduct} from '../components'
-
-const ProductCard = ({ jwt , product, user, navigate}) => {
-const { id, title, imageurl, description, price, author, genre } = product;
-console.log('THIS product', id)
-const [qty, setQty] = useState(1);
-import { addProductToCart, deleteProduct } from '../api';
 import { Card, ListGroup, Button, Tab, Tabs, Container, Row, Col, Modal, Alert } from 'react-bootstrap';
 import { EditProduct } from '../components';
 // import { Link } from 'react-router-dom';
@@ -15,11 +7,6 @@ import { EditProduct } from '../components';
 const ProductCard = ({ jwt, product, user, allProducts, navigate }) => {
   const { id, title, imageurl, description, price, author, genre } = product;
   const [qty, setQty] = useState(1);
-
-const [show, setShow] = useState(false);
-const handleClose = () => setShow(false);
-const [successMessage, setSuccessMessage] = useState('');
-const [errorMessage, setErrorMessage] = useState('');
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -60,19 +47,6 @@ const [errorMessage, setErrorMessage] = useState('');
   };
 
 
-const [showModal, setShowModal] = useState(false);
-const openModal = () => setShow(true);
-
-
-const handleShow = () => {
-  setErrorMessage('');
-  setSuccessMessage('');
-  setShowModal(true);
-};
-
-const closeModal = () => {
-  setShowModal(false);
-};
   async function handleDelete() {
     const result = await deleteProduct(jwt, id);
     if (result) {
@@ -90,102 +64,6 @@ const closeModal = () => {
 
   return (
 
-<ListGroup.Item
-    key={id}
-    className='px-3 py-3 mx-3'>
-    <Card.Text>
-      <Container >
-      <span className='ms-1 pt-4 d-flex justify-content-start' style={{ fontSize: '30px' }}>
-      {title}
-      </span>
-      <span className='ms-1 pb-1 d-flex justify-content-start'>by {author}</span>
-      <Row>
-    <Col className='pt-2'xs lg="3">
-    <img src={imageurl} className='pb-3'/>
-    </Col>
-      <Col>
-      <Tabs
-      defaultActiveKey="price"
-      id="uncontrolled-tab-example"
-      className='mx-3 pt-0 d-flex justify-content-end'>
-      <Tab className='pe-4 pt-2 float-end' eventKey="description" title="Description">
-        <Button variant="light" onClick={() => {
-          openModal();
-        }}>Read Full Description</Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header style={{ fontSize: '20px' }} closeButton>
-          {title}, by {author}
-        </Modal.Header>
-        <Modal.Body>
-          {description}
-        </Modal.Body>
-      </Modal>
-      </Tab>
-      <Tab className='pe-4 pt-2 float-end' eventKey="genre" title="Genre">
-      {genre}
-      </Tab>
-      <Tab className='pe-4 pt-2 float-end' eventKey="price" title="Price">
-      <h4 >${price}</h4>
-      </Tab>
-      </Tabs>
-      
-     {jwt ? (
-      <>
-       <Card.Text className='mt-5 pt-5 pb-4 d-flex justify-content-end flex-wrap'>
-       <input className='me-2' id='input' min="1" max="100" type="number" value={qty} 
-       onChange={(event) => setQty(event.target.value)}></input>
-        <Button variant="outline-primary" 
-        onClick={event => {
-          event.preventDefault();
-                addToCart();
-          }}>
-          Add to Cart
-        </Button>
-         </Card.Text>
-         <Card.Text className='d-flex justify-content-end flex-wrap'>
-      {successMessage && (
-        <Alert id='addprodalert' variant='success'>
-          {successMessage}
-        </Alert>
-      )}
-      </Card.Text>
-      </>
-     ) : null
-      }
-      {user.isAdmin ? (
-      <>
-      <Card.Text className='d-flex justify-content-end flex-wrap'>
-        <Button className='mx-2' variant="danger" 
-        onClick={() => {
-          if (confirm('Are you sure you want to delete?')) {
-            deleteProduct(jwt, id);
-            console.log('delete button pressed')
-            }
-          }}>
-        Delete
-      </Button>
-      <Button variant="outline-info" 
-        onClick={event => {
-          event.preventDefault();
-                handleShow();}}
-        >
-        Edit
-      </Button>
-      <>
-      <Modal show={showModal} onHide={closeModal}>
-        <Modal.Header style={{ fontSize: '20px' }} closeButton>
-          <Modal.Title className='w-100 text-center'>Update Product</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <EditProduct jwt={jwt} product={product}/>
-        </Modal.Body>
-        </Modal>
-    </>
-      </Card.Text>
-     </> 
-     ) : null } 
-      </Col></Row>
-      </Container>
     <ListGroup.Item
       key={id}
       className='px-3 py-3 mx-3'>
@@ -294,11 +172,6 @@ const closeModal = () => {
               {errorMessage}
             </Alert>
           )}
-          {successMessage && (
-            <Alert variant='success' className='mt-3'>
-              {successMessage}
-            </Alert>
-          )}
         </Container>
       </Card.Text>
     </ListGroup.Item>
@@ -306,16 +179,3 @@ const closeModal = () => {
 };
 
 export default ProductCard;
-
-
-
-// {user.isAdmin ? (
-//     <div>
-//      <input type="number" value={qty} onChange={(event) => setQty(event.target.value)}></input>
-//       <Button variant='info' onClick={async () => {
-//           const product = await addProductToCart(jwt, { productId: id, qty })
-//       }
-          
-//           }>Add to Cart</Button>
-//     </div>) : null
-//   }
